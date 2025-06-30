3fda4c3983477a668c16702d53a9c7f3b616a7ce
ZrefiXiord <zrefixiord@gmail.com>
2025-06-30 19:16:58 +0200

    Add service bindings for Supplier, Stock, StockTransaction, PurchaseOrder, and User services in AppServiceProvider to enhance dependency injection and service management.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,6 +6,11 @@ use App\Services\CategoryService;
 use App\Services\DashboardService;
 use App\Services\LocationService;
 use App\Services\ProductService;
+use App\Services\PurchaseOrderService;
+use App\Services\StockService;
+use App\Services\StockTransactionService;
+use App\Services\SupplierService;
+use App\Services\UserService;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -20,6 +25,11 @@ class AppServiceProvider extends ServiceProvider
         $this->app->singleton(ProductService::class);
         $this->app->singleton(LocationService::class);
         $this->app->singleton(DashboardService::class);
+        $this->app->singleton(SupplierService::class);
+        $this->app->singleton(StockService::class);
+        $this->app->singleton(StockTransactionService::class);
+        $this->app->singleton(PurchaseOrderService::class);
+        $this->app->singleton(UserService::class);
     }
 
     /**
